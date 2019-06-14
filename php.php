d2b41ef7e53ca3e34d4160cfd250f3b1e22434c9
dtlnor <dtlnor@hotmail.com>
2019-06-14 23:26:39 +0800

    c
    c

--- php.php
+++ php.php
@@ -3,7 +3,7 @@ $BOT_TOKEN = "770506138:AAGSSp_BOuvmlzWY1cBdrsApMv4zL5dbYa8";
 $API_URL = "https://api.telegram.org/bot".$BOT_TOKEN;
  
 // read incoming info and grab the chatID
-$content = file_get_contents($API_URL."/getupdates");
+$content = file_get_contents("php://input");
 $update = json_decode($content, true);
 $chatID = $update["message"]["chat"]["id"];
 $got_message = $update["message"]["text"];
@@ -12,6 +12,6 @@ $got_message = $update["message"]["text"];
 $reply =  $got_message;
   
 // send reply
-$sendto =API_URL."sendmessage?chat_id=".$chatID."&text=".$reply;
+$sendto =$API_URL."/sendmessage?chat_id=".$chatID."&text=".$reply;
 file_get_contents($sendto);
 ?>
